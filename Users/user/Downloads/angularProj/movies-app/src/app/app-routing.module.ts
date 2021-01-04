ba8f3b184d5296115bb3382774d6cbf088389030
carmelkt <72968944+carmelkt@users.noreply.github.com>
2021-01-04 21:55:30 +0530

    Update app-routing.module.ts

--- Users/user/Downloads/angularProj/movies-app/src/app/app-routing.module.ts
+++ Users/user/Downloads/angularProj/movies-app/src/app/app-routing.module.ts
@@ -15,25 +15,19 @@ import { MoviesResolverService } from './movies/movies-resolver.service';
 import { MoviesComponent } from './movies/movies.component';
 
 const appRoutes:Routes=[
-    {path:'', redirectTo:'/auth/login',pathMatch:'full'},
-
-    // {path:'header',component:HeaderComponent},
-
-{path:'movies', component:MoviesComponent,resolve:[MoviesResolverService], children:[
-    {path:'',component:MovieStartComponent},
-    {path:'new',component:MovieEditComponent,canActivate:[AuthGuardGuard],data:{permittedRoles:['Admin']}},
-    {path:':id', component:MovieDetailComponent, resolve:[MoviesResolverService]},
-    {path:':id/edit', component:MovieEditComponent, resolve:[MoviesResolverService],canActivate:[AuthGuardGuard],data :{permittedRoles:['Admin']}}  
-],canActivate:[AuthGuardGuard]},
-//{path:'header',component:HeaderComponent,canActivate:[AuthGuardGuard]},
-  {path:'forbidden',component:ForbiddenComponent},
-  //{path:'adminpanel',component:AdminPanelComponent,canActivate:[AuthGuardGuard],data :{permittedRoles:['Admin']}},
-{path:'auth',component:AuthComponent, children:[
-    {path:'',component:LoginComponent},
-    {path:'registration',component:RegistrationComponent},
-    {path:'login',component:LoginComponent}
-]}
-];
+    {path:'', redirectTo:'/auth/login',pathMatch:'full'},    
+    {path:'movies', component:MoviesComponent,resolve:[MoviesResolverService], children:[
+       {path:'',component:MovieStartComponent},
+       {path:'new',component:MovieEditComponent,canActivate:[AuthGuardGuard],data:{permittedRoles:['Admin']}},
+       {path:':id', component:MovieDetailComponent, resolve:[MoviesResolverService]},
+       {path:':id/edit', component:MovieEditComponent, resolve:[MoviesResolverService],canActivate:[AuthGuardGuard],data :{permittedRoles:['Admin']}}  
+     ],canActivate:[AuthGuardGuard]},
+    {path:'forbidden',component:ForbiddenComponent},
+    {path:'auth',component:AuthComponent, children:[
+       {path:'',component:LoginComponent},
+       {path:'registration',component:RegistrationComponent},
+       {path:'login',component:LoginComponent}
+     ]}];
 
 @NgModule({
 imports:[RouterModule.forRoot(appRoutes)],
@@ -41,5 +35,4 @@ exports:[RouterModule]
 })
 
 export class appRoutingModule{
-
 }
