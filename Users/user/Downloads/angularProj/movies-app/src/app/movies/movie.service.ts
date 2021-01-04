ef15d01662de9c10f13417a83ef4a9928ef6a51d
carmelkt <72968944+carmelkt@users.noreply.github.com>
2021-01-04 21:46:26 +0530

    Update movie.service.ts

--- Users/user/Downloads/angularProj/movies-app/src/app/movies/movie.service.ts
+++ Users/user/Downloads/angularProj/movies-app/src/app/movies/movie.service.ts
@@ -1,32 +1,13 @@
-
-
 import { Subject } from 'rxjs';
-
 import { Movie } from './movie.model';
 
-
 export class MovieService{
-    moviesChanged=new Subject<Movie[]>();
-
-
-    // private movies:Movie[]=[
-    //   new Movie('KalyanaRaman',
-    //  'Old but good Malayalam MOVIE!',
-    //  'https://www.whykol.com/wk-uploads/movieposters/kalyaanaraaman-3452-poster.jpg',
-    // 'dileep, navya nair'),
-    //      new Movie('Vettam',
-    //      'Old but good Malayalam MOVIE!',
-    //      'https://i.ytimg.com/vi/40nfhLC7i84/maxresdefault.jpg',
-    //     'innocent, mani')
-    //   ]; 
-
-    //private movies:Movie[]=[];
+    moviesChanged=new Subject<Movie[]>();   
     private movies:Movie[]=[];
-
+  
       setMovies(movies:Movie[]){
           this.movies=movies;
           this.moviesChanged.next(this.movies.slice());
-
       }
 
       getMovies(){
@@ -38,18 +19,17 @@ export class MovieService{
       }
 
       addMovie(movie:Movie){
-this.movies.push(movie);
-this.moviesChanged.next(this.movies.slice());
+       this.movies.push(movie);
+       this.moviesChanged.next(this.movies.slice());
       }
 
       updateMovie(index:number,newMovie:Movie){
-this.movies[index]=newMovie;
-this.moviesChanged.next(this.movies.slice());
+       this.movies[index]=newMovie;
+       this.moviesChanged.next(this.movies.slice());
       }
 
       deleteMovie(index:number){
-          this.movies.splice(index,1);
-          
-          this.moviesChanged.next(this.movies.slice());
+        this.movies.splice(index,1);          
+        this.moviesChanged.next(this.movies.slice());
       }
 }
