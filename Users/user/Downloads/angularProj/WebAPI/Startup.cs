b1f15ad38bf9563fdb429221877aaf293ad18ade
carmelkt <72968944+carmelkt@users.noreply.github.com>
2021-01-04 22:06:39 +0530

    Update Startup.cs

--- Users/user/Downloads/angularProj/WebAPI/Startup.cs
+++ Users/user/Downloads/angularProj/WebAPI/Startup.cs
@@ -58,24 +58,22 @@ namespace WebAPI
 
             var key = Encoding.UTF8.GetBytes(Configuration["ApplicationSettings:JWT_Secret"].ToString());
 
-            services.AddAuthentication(x=>
-            {
-                x.DefaultAuthenticateScheme=JwtBearerDefaults.AuthenticationScheme;
-                 x.DefaultChallengeScheme=JwtBearerDefaults.AuthenticationScheme;
+            services.AddAuthentication(x=>{
+                  x.DefaultAuthenticateScheme=JwtBearerDefaults.AuthenticationScheme;
+                  x.DefaultChallengeScheme=JwtBearerDefaults.AuthenticationScheme;
                   x.DefaultScheme=JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(x=>{
-                x.RequireHttpsMetadata=false;
-                x.SaveToken=false;
-                x.TokenValidationParameters=new Microsoft.IdentityModel.Tokens.TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey=true,
-                    IssuerSigningKey=new SymmetricSecurityKey(key),
-                    ValidateIssuer=false,
-                    ValidateAudience=false,
-                    ClockSkew=TimeSpan.Zero                 
+                  }).AddJwtBearer(x=>{
+                      x.RequireHttpsMetadata=false;
+                      x.SaveToken=false;
+                      x.TokenValidationParameters=new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                        {
+                          ValidateIssuerSigningKey=true,
+                          IssuerSigningKey=new SymmetricSecurityKey(key),
+                          ValidateIssuer=false,
+                          ValidateAudience=false,
+                          ClockSkew=TimeSpan.Zero                 
                 };
             });
-
             services.AddScoped<IMovieRepository,MovieRepository>();
         }
 
