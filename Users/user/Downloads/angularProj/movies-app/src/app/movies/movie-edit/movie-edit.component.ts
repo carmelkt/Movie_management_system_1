a7aa2033b8c45a577720e7d224667025594ebfce
carmelkt <72968944+carmelkt@users.noreply.github.com>
2021-01-04 21:42:14 +0530

    Update movie-edit.component.ts

--- Users/user/Downloads/angularProj/movies-app/src/app/movies/movie-edit/movie-edit.component.ts
+++ Users/user/Downloads/angularProj/movies-app/src/app/movies/movie-edit/movie-edit.component.ts
@@ -21,59 +21,36 @@ export class MovieEditComponent implements OnInit {
     private dsService:DataStorageService) { }
 
   ngOnInit(){
-this.route.params.subscribe(
-  (params:Params)=>{
+    this.route.params.subscribe(
+    (params:Params)=>{
     this.id=+params['id'];
     this.editMode=params['id'] !=null;
     this.initForm();
+    });  
   }
-);
-
-
-  
-}
 
 
   onSubmit(){
    if(this.editMode){
      this.movieService.updateMovie(this.id,this.movieForm.value);
-   } else{
+   } 
+   else{
      this.movieService.addMovie(this.movieForm.value);
    }
-   this.onCancel();
-   this.dsService.storeMovies();
-  }
-
-//   onAddActor(){
-//     (<FormArray>this.movieForm.get('actors')).push(
-//       new FormGroup({
-//         'name':new FormControl(null,Validators.required),
-//         'role':new FormControl(null,Validators.required)
-//       })
-//     )
-//   }
-
-//   onDeleteActor(index:number){
-// (<FormArray>this.movieForm.get('actors')).removeAt(index);
-//   }
+    this.onCancel();
+    this.dsService.storeMovies();
+   }
 
   onCancel(){
-this.router.navigate(['../'],{relativeTo:this.route});
+    this.router.navigate(['../'],{relativeTo:this.route});
   }
 
-  // saveData(){
-  //   this.dsService.storeMovies();
-  // }
-
-  
-
   private initForm(){
-let movieName='';
-let movieImagePath='';
-let movieDescription='';
-let movieActors='';
-let movieID=0;
-//let movieActors=new FormArray([]);
+     let movieName='';
+     let movieImagePath='';
+     let movieDescription='';
+     let movieActors='';
+     let movieID=0;
 
 if(this.editMode){
   const movie=this.movieService.getMovie(this.id);
@@ -82,17 +59,6 @@ if(this.editMode){
   movieDescription=movie.description;
   movieActors=movie.actors;
   movieID=movie.id;
-  
-//  if(movie['actors']){
-//     for(let actor of movie.actors){
-//       movieActors.push(
-//         new FormGroup({
-//           'name':new FormControl(actor.name,Validators.required),
-//           'role':new FormControl(actor.role,Validators.required)
-//         })
-//       );
-//     }
-//   }
 }
     this.movieForm=new FormGroup({
       'name':new FormControl(movieName,Validators.required),
@@ -100,15 +66,6 @@ if(this.editMode){
       'description':new FormControl(movieDescription,Validators.required),
       'actors':new FormControl(movieActors,Validators.required),
       'id':new FormControl(movieID)
-      //'actors':movieActors
     });
-    
-
   }
-  
-  // get controls() { // a getter!
-  //   return (<FormArray>this.movieForm.get('actors')).controls;
-  // } 
-
-
 }
