370baa9eee2e32e9f5e535e3852973b2833e4efb
carmelkt <72968944+carmelkt@users.noreply.github.com>
2021-01-04 21:48:17 +0530

    Update movies.component.ts

--- Users/user/Downloads/angularProj/movies-app/src/app/movies/movies.component.ts
+++ Users/user/Downloads/angularProj/movies-app/src/app/movies/movies.component.ts
@@ -5,15 +5,11 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-movies',
   templateUrl: './movies.component.html',
   styleUrls: ['./movies.component.css']
- 
 })
 export class MoviesComponent implements OnInit {
  
-
   constructor() { }
 
-  ngOnInit() {
-    
+  ngOnInit() {   
   }
-
 }
